fea0530f5700f2953556f3de6ee6cd9ba8481274
Kali Live user <wiktorbigaj@wp.pl>
2020-06-06 14:00:53 +0000

    Added checkPasswordRules and checkPassword and placeholders for Dawid's functions

--- module3/homework/passwordCheck/validation.hpp
+++ module3/homework/passwordCheck/validation.hpp
@@ -1,2 +1,36 @@
-// TODO: I'm empty :) Put enum and function headers here.
-// Don't forget the header guard
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <time.h>
+
+enum ErrorCode
+{
+    Ok,
+    PasswordNeedsAtLeastNineCharacters,
+    PasswordNeedsAtLeastOneNumber,
+    PasswordNeedsAtLeastOneSpecialCharacter,
+    PasswordNeedsAtLeastOneUppercaseLetter,
+    PasswordsDoesNotMatch
+};
+
+std::string getErrorMessage(ErrorCode error){
+    return "Ok";
+}
+
+bool doesPasswordsMatch(std::string input,std::string password){
+    return false;
+}
+
+std::string checkPasswordRules(const std::string& password){
+   srand (time(NULL));
+   int random = rand() % 6;
+   return getErrorMessage(ErrorCode(random));
+}
+
+std::string checkPassword(std::string input,std::string password){
+    if (!doesPasswordsMatch(input,password)) {
+        return getErrorMessage(ErrorCode::PasswordsDoesNotMatch);
+    }
+    return checkPasswordRules(input);
+}
+
